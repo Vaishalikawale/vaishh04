31b7285052ce6a6de138913685198e58e18349ff
vaishalikawale2005@gmail.com <vaishalikawale2005@gamil.com>
2026-02-04 14:48:38 -0800

    prog1commit

---
+++ CV.js
@@ -0,0 +1,3 @@
+Education - 10th completed in SMIGS high school
+          - 12th completed in ABVRC Bidar
+          - Now B.E studying in SKIT Banglore
---
+++ CV.py
@@ -0,0 +1,1 @@
+Hi, This is Vaishali Kawale, studying 3rd year as a computer Science and Engineering
